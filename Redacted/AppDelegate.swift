57c9a8a34f3236b9c46e9c25f4daf1918adbfd5a
ollyroo <ollyroo97@gmail.com>
2015-02-05 08:47:55 +1100

    Initial Commit

---
+++ Redacted/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Redacted
+//
+//  Created by Olly Clissold on 5/02/2015.
+//  Copyright (c) 2015 CreatingCrap. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Redacted/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Redacted
+//
+//  Created by Olly Clissold on 5/02/2015.
+//  Copyright (c) 2015 CreatingCrap. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
