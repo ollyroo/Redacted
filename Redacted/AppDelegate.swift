2cfb28ce70f504bd5178b2a397c1fd6627505e45
ollyroo <ollyroo97@gmail.com>
2015-05-13 14:39:04 +1000

    reenabling compiling

--- Redacted/AppDelegate.swift
+++ Redacted/AppDelegate.swift
@@ -10,18 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-<<<<<<< HEAD
-=======
     
->>>>>>> ad39c077b9fc42389ce80d86dc8a95c3c6f5b508
-
 }
-
